fea622882f8d695b07685b52a1c73b7f07e11433
Ilya Borshchevskiy <ilya.borshchevskiy@gmail.com>
2022-04-01 09:37:07 +0300

    JUnit-5 practice added

---
+++ JUnit5-Practice/src/test/java/com/dmdev/junit/TestLauncher.java
@@ -0,0 +1,37 @@
+package com.dmdev.junit;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+//        launcher.registerLauncherDiscoveryListeners();
+
+        var summaryGeneratingListener = new SummaryGeneratingListener();
+//        launcher.registerTestExecutionListeners();
+//        launcher.registerTestExecutionListeners(summaryGeneratingListener);
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                .selectors(DiscoverySelectors.selectPackage("com.dmdev.junit.service"))
+                .filters(
+                        TagFilter.excludeTags("login")
+                )
+//                .listeners()
+                .build();
+        launcher.execute(request, summaryGeneratingListener);
+
+        try (var writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
---
+++ JUnit5-Practice/src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -0,0 +1,293 @@
+package com.dmdev.junit.service;
+
+import com.dmdev.junit.TestBase;
+import com.dmdev.junit.dao.UserDao;
+import com.dmdev.junit.dto.User;
+import com.dmdev.junit.extension.ConditionalExtension;
+import com.dmdev.junit.extension.PostProcessingExtension;
+import com.dmdev.junit.extension.UserServiceParamResolver;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@Tag("fast")
+@Tag("user")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+@ExtendWith({
+        UserServiceParamResolver.class,
+        PostProcessingExtension.class,
+        ConditionalExtension.class,
+        MockitoExtension.class
+//        ThrowableExtension.class
+//        GlobalExtension.class
+})
+//@RunWith()
+class UserServiceTest extends TestBase {
+
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+
+//    @Rule
+//    ExpectedException
+
+    @Captor
+    private ArgumentCaptor<Integer> argumentCaptor;
+    @Mock(lenient = true)
+    private UserDao userDao;
+    @InjectMocks
+    private UserService userService;
+
+    UserServiceTest(TestInfo testInfo) {
+        System.out.println();
+    }
+
+    @BeforeAll
+    void init() {
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+//        userService.userDao = Mockito.mock()
+//        lenient().when(userDao.delete(IVAN.getId())).thenReturn(true);
+//        Mockito.mockStatic()
+
+        doReturn(true).when(userDao).delete(IVAN.getId());
+//        Mockito.mock(UserDao.class, withSettings().lenient())
+//        this.userDao = Mockito.spy(new UserDao());
+//        this.userService = new UserService(userDao);
+    }
+
+    @Test
+    void throwExceptionIfDatabaseIsNotAvailable() {
+        doThrow(RuntimeException.class).when(userDao).delete(IVAN.getId());
+
+        assertThrows(RuntimeException.class, () -> userService.delete(IVAN.getId()));
+    }
+
+    @Test
+    void shouldDeleteExistedUser() {
+        userService.add(IVAN);
+//        Mockito.doReturn(true).when(userDao).delete(IVAN.getId());
+//        Mockito.doReturn(true).when(userDao).delete(Mockito.any());
+
+//        Mockito.when(userDao.delete(IVAN.getId()))
+//                .thenReturn(true)
+//                .thenReturn(false);
+
+//        BDDMockito.given(userDao.delete(IVAN.getId())).willReturn(true);
+
+//        BDDMockito.willReturn(true).given(userDao).delete(IVAN.getId());
+
+        var deleteResult = userService.delete(IVAN.getId());
+        System.out.println(userService.delete(IVAN.getId()));
+        System.out.println(userService.delete(IVAN.getId()));
+
+//        var argumentCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(userDao, Mockito.times(3)).delete(argumentCaptor.capture());
+//        Mockito.ve
+        assertThat(argumentCaptor.getValue()).isEqualTo(IVAN.getId());
+
+//        Mockito.reset(userDao);
+
+        assertThat(deleteResult).isTrue();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("users will be empty if no user added")
+    void usersEmptyIfNoUserAdded(UserService userService) throws IOException {
+//        if (true) {
+//            throw new RuntimeException();
+//        }
+        System.out.println("Test 1: " + this);
+        var users = userService.getAll();
+
+        MatcherAssert.assertThat(users, empty());
+        assertTrue(users.isEmpty(), () -> "User list should be empty");
+//        input -> [box == func] -> actual output
+    }
+
+    @Test
+    @Order(2)
+    void usersSizeIfUserAdded() {
+//        given
+//        when
+//        then
+//        when
+//        then
+//        when
+//        then
+        System.out.println("Test 2: " + this);
+        userService.add(IVAN);
+        userService.add(PETR);
+
+        var users = userService.getAll();
+
+        assertThat(users).hasSize(5);
+//        assertEquals(2, users.size());
+    }
+
+    @Test
+    void usersConvertedToMapById() {
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
+        assertAll(
+                () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+                () -> assertThat(users).containsValues(IVAN, PETR)
+        );
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
+    }
+
+    @Nested
+    @DisplayName("test user login functionality")
+    @Tag("login")
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    class LoginTest {
+
+        @Test
+        @Disabled("flaky, need to see")
+        void loginFailIfPasswordIsNotCorrect() {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        //        @Test
+        @RepeatedTest(value = 5, name = RepeatedTest.LONG_DISPLAY_NAME)
+        void loginFailIfUserDoesNotExist(RepetitionInfo repetitionInfo) {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        void checkLoginFunctionalityPerformance() {
+            System.out.println(Thread.currentThread().getName());
+            var result = assertTimeoutPreemptively(Duration.ofMillis(200L), () -> {
+                System.out.println(Thread.currentThread().getName());
+                Thread.sleep(100L);
+                return userService.login("dummy", IVAN.getPassword());
+            });
+        }
+
+        @Test
+        void loginSuccessIfUserExists() {
+            userService.add(IVAN);
+
+            Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+//        assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+        }
+
+        @Test
+//    @org.junit.Test(expected = IllegalArgumentException.class)
+        void throwExceptionIfUsernameOrPasswordIsNull() {
+            assertAll(
+                    () -> {
+                        var exception = assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy"));
+                        assertThat(exception.getMessage()).isEqualTo("username or password is null");
+                    },
+                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
+            );
+        }
+
+        @ParameterizedTest(name = "{arguments} test")
+//        @ArgumentsSource()
+//        @NullSource
+//        @EmptySource
+//        @NullAndEmptySource
+//        @ValueSource(strings = {
+//                "Ivan", "Petr"
+//        })
+//        @EnumSource
+        @MethodSource("com.dmdev.junit.service.UserServiceTest#getArgumentsForLoginTest")
+//        @CsvFileSource(resources = "/login-test-data.csv", delimiter = ',', numLinesToSkip = 1)
+//        @CsvSource({
+//                "Ivan,123",
+//                "Petr,111"
+//        })
+        @DisplayName("login param test")
+        void loginParameterizedTest(String username, String password, Optional<User> user) {
+            userService.add(IVAN, PETR);
+
+            var maybeUser = userService.login(username, password);
+            assertThat(maybeUser).isEqualTo(user);
+        }
+
+
+    }
+
+    static Stream<Arguments> getArgumentsForLoginTest() {
+        return Stream.of(
+                Arguments.of("Ivan", "123", Optional.of(IVAN)),
+                Arguments.of("Petr", "111", Optional.of(PETR)),
+                Arguments.of("Petr", "dummy", Optional.empty()),
+                Arguments.of("dummy", "123", Optional.empty())
+        );
+    }
+}
