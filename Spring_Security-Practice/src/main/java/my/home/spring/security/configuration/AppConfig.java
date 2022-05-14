e8a752393fad3033d6a49871d2bab2fdaae61a2a
Ilya Borshchevskiy <ilya.borshchevskiy@gmail.com>
2022-05-14 22:42:11 +0300

    added Spring Security practice

---
+++ Spring_Security-Practice/src/main/java/my/home/spring/security/configuration/AppConfig.java
@@ -0,0 +1,45 @@
+package my.home.spring.security.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+
+@Configuration
+@ComponentScan("my.home.spring.security")
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try{
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/security");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("postgres");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+
+
+}
---
+++ Spring_Security-Practice/src/main/webapp/WEB-INF/view/view_for_managers.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Performance</title>
+</head>
+<body>
+    <h3>Here you can see employees' performance</h3>
+</body>
+</html>
