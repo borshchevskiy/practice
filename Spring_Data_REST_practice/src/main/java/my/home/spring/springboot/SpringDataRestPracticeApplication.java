86d28931045d21d4f25d9dfd9dd250bdd93cee10
Ilya Borshchevskiy <ilya.borshchevskiy@gmail.com>
2022-05-20 16:28:50 +0300

    added Spring Data REST practice

---
+++ Spring_Data_REST_practice/src/main/java/my/home/spring/springboot/SpringDataRestPracticeApplication.java
@@ -0,0 +1,13 @@
+package my.home.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestPracticeApplication.class, args);
+    }
+
+}
---
+++ Spring_Data_REST_practice/src/test/java/my/home/spring/springboot/spring_data_rest_practice/SpringDataRestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package my.home.spring.springboot.spring_data_rest_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
